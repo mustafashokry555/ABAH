bb6f2d501b4f7deb537f10ee3a10be644156793b
mustafashokry555 <mustafashokry555@gmail.com>
2024-01-05 15:14:28 +0200

    appiontment

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -11,9 +11,10 @@ use Illuminate\Support\Facades\Validator;
 
 class PatientController extends Controller
 {
-    function insurance(Request $request) {
+    function insurance(Request $request)
+    {
 
-        if(!isset($request->user()->PatientId)){
+        if (!isset($request->user()->PatientId)) {
             return response()->json(['errors' => 'The patient id field is required.', 'status' => 422]);
         }
         try {
@@ -27,7 +28,8 @@ class PatientController extends Controller
         }
     }
 
-    function radiologyReports(Request $request) {
+    function radiologyReports(Request $request)
+    {
         try {
             $data = DB::select('usp_apiGetAllRadioDatewise ?, ?', [
                 $request->user()->PatientId,
@@ -40,7 +42,8 @@ class PatientController extends Controller
         }
     }
 
-    function labResults(Request $request) {
+    function labResults(Request $request)
+    {
         try {
             $data = DB::select('usp_app_apigetLabPatwiseResults ?', [
                 $request->user()->PatientId,
@@ -52,21 +55,23 @@ class PatientController extends Controller
         }
     }
 
-    function appointment(Request $request) {
+    function appointment(Request $request)
+    {
         try {
             $PatientId = $request->user()->PatientId;
             $appointments = DB::table("Ds_PatientAppoinmentTemperary")
-            ->where([
-                'PatientID' => $PatientId,
-            ])->get();
-            return response()->json(['data' => $appointments ,'status' => 200]);
+                ->where([
+                    'PatientID' => $PatientId,
+                ])->get();
+            return response()->json(['data' => $appointments, 'status' => 200]);
         } catch (\Throwable $th) {
             // return $th;
             return response()->json(['errors' => 'Database Error !', 'status' => 500]);
         }
     }
 
-    function prescriptions(Request $request) {
+    function prescriptions(Request $request)
+    {
         try {
             $data = DB::select('usp_app_apiGetAllPrescriptions ?, ?', [
                 $request->user()->PatientId,
@@ -79,7 +84,8 @@ class PatientController extends Controller
         }
     }
 
-    function bills(Request $request) {
+    function bills(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'month' => 'required',
             'year' => 'required',
@@ -99,11 +105,11 @@ class PatientController extends Controller
         } catch (\Throwable $th) {
             // return $th;
             return response()->json(['errors' => 'Database Error !', 'status' => 500]);
-
         }
     }
 
-    function visit_history(Request $request) {
+    function visit_history(Request $request)
+    {
 
         try {
             $data = DB::select('usp_app_api_GetAllVisitList ?, ?', [
@@ -115,66 +121,55 @@ class PatientController extends Controller
         } catch (\Throwable $th) {
             // return $th;
             return response()->json(['errors' => 'Database Error !', 'status' => 500]);
-
         }
     }
 
-    function myRate(Request $request) {
+    function myRate(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'doctor_id' => 'required|exists:Employee_Mst,EmpID,Deactive,0',
         ]);
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors(), 'status' => 422]);
         }
-        try{
+        try {
             $PatientId = $request->user()->PatientId;
             $rate = DB::table("app_rate_doctors")
-            ->where([
-                'patient_id' => $PatientId,
-                'doctor_id' => $request->doctor_id
-            ])->first();
-            return response()->json(['data' => $rate ,'status' => 200]);
+                ->where([
+                    'patient_id' => $PatientId,
+                    'doctor_id' => $request->doctor_id
+                ])->first();
+            return response()->json(['data' => $rate, 'status' => 200]);
         } catch (\Throwable $th) {
             // return $th;
             return response()->json(['errors' => 'Database Error !', 'status' => 500]);
         }
     }
 
-    function makeAppointment(Request $request) {
-        //validate important inputs
-        $validator = Validator::make($request->all(), [
-            'doctor_id' => 'required|exists:Employee_Mst,EmpID,Deactive,0',
-            'DATE' => 'required|date',
-            'SlotsTime' => [
-                'required',
-                'regex:/^(0[1-9]|1[0-2]):[0-5][0-9] (AM|PM) To (0[1-9]|1[0-2]):[0-5][0-9] (AM|PM)$/'
-            ],
-        ]);
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors(), 'status' => 422]);
-        }
+    function addTheAppoint(Request $request)
+    {
         // get from & to dateTime
         list($fromTime, $toTime) = explode(" To ", $request->SlotsTime);
         $date = new DateTime($request->DATE);
         $from = $date->format('Y-m-d') . ' ' . date('H:i:s', strtotime($fromTime));
         $to = $date->format('Y-m-d') . ' ' . date('H:i:s', strtotime($toTime));
         $AppointmentCount = DB::table('Ds_PatientAppoinmentTemperary')
-        ->where([
-            'FromTime' => $from,
-            'ToTime' => $to,
-            'DoctorID' => $request->doctor_id,
-            'ApntStatus' => 'CONFIRMED'
-        ])
-        // ->whereNull('Cancelled')
-        ->count();
-        if($AppointmentCount > 0){
-            return response()->json(['message'=>"This time slot is already booked.", 'status' => 406]);
+            ->where([
+                'FromTime' => $from,
+                'ToTime' => $to,
+                'DoctorID' => $request->doctor_id,
+                'ApntStatus' => 'CONFIRMED'
+            ])
+            // ->whereNull('Cancelled')
+            ->count();
+        if ($AppointmentCount > 0) {
+            return response()->json(['message' => "This time slot is already booked.", 'status' => 406]);
         }
         // return $AppointmentCount;
 
         // generate new AppointmentCode
         $lastAppointmentCode = DB::table('Ds_PatientAppoinmentTemperary')
-        ->orderBy('ID', 'DESC')->get(['ID', 'AppointmentCode'])->first();
+            ->orderBy('ID', 'DESC')->get(['ID', 'AppointmentCode'])->first();
         $newCode = substr(strrchr($lastAppointmentCode->AppointmentCode, "/"), 1) + 1;
         $newAppointmentCode = 'WA/' . $date->format('Ym') . '/' . $newCode;
 
@@ -186,7 +181,7 @@ class PatientController extends Controller
             'DoctorID' => $request->doctor_id,
             'Duration' => 0,
             'Loc_Id' => 1,
-            'app_date' => $date->format('Y-m-d')." 00:00:00.000",
+            'app_date' => $date->format('Y-m-d') . " 00:00:00.000",
         ];
 
         $user = Auth::guard('api')->user();
@@ -195,7 +190,7 @@ class PatientController extends Controller
             // merge user data in row array 
             $row = array_merge($row, [
                 "PatientID" => $user->PatientId,
-                "Firstname"=> $user->First_Name,
+                "Firstname" => $user->First_Name,
                 "Middlename" => $user->Middle_Name,
                 "LastName" => $user->Last_Name,
                 "Age" => $user->Age,
@@ -226,7 +221,7 @@ class PatientController extends Controller
             // case the patient is Gest
             $row = array_merge($row, [
                 "PatientID" => 0,
-                "Firstname"=> $request->First_Name,
+                "Firstname" => $request->First_Name,
                 "Middlename" => $request->Middle_Name,
                 "LastName" => $request->Last_Name,
                 "Age" => $request->Age,
@@ -240,15 +235,63 @@ class PatientController extends Controller
         }
         try {
             $newAppointment = DB::table('Ds_PatientAppoinmentTemperary')
-            ->insert($row);
-            return response()->json(['message'=>'Your Appointment has been Updated Successfully!', 'status' => 200]);
+                ->insert($row);
+            return response()->json(['message' => 'Your Appointment has been Updated Successfully!', 'status' => 200]);
         } catch (\Throwable $th) {
             throw $th;
             return response()->json(['errors' => 'Database Error !', 'status' => 500]);
         }
     }
+    function makeAppointment(Request $request)
+    {
+        //validate important inputs
+        $validator = Validator::make($request->all(), [
+            'doctor_id' => 'required|exists:Employee_Mst,EmpID,Deactive,0',
+            'DATE' => 'required|date',
+            'SlotsTime' => [
+                'required',
+                'regex:/^(0[1-9]|1[0-2]):[0-5][0-9] (AM|PM) To (0[1-9]|1[0-2]):[0-5][0-9] (AM|PM)$/'
+            ],
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors(), 'status' => 422]);
+        }
 
-    function cancelAppointment(Request $request) {
+        $avilSlots = DB::select('usp_app_apiGetDocAppSlots ?, ?, ?, ?', [
+            $request->doctor_id,
+            $request->DATE,
+            $request->DATE,
+            '1'
+        ]);
+        if (!empty($avilSlots)) {
+            $SlotTime = $request->SlotsTime;
+            $avilSlot = collect($avilSlots)->filter(function ($item) use ($SlotTime) {
+                return $item->SlotsTime == $SlotTime;
+            })->values()->first();
+            if (!empty($avilSlot)) {
+                if (empty(trim($avilSlot->sub)) && $avilSlot->PatientId == 0) {
+                    // continue the booking
+                    return $this->addTheAppoint($request);
+                }else{
+                    // show message that slot is not available
+                    return response()->json([
+                        "message" => "This Slot is already Booked by Someone else",
+                        "status" => 406
+                    ]);
+                }
+            } else {
+                return response()->json(['errors' => ['No Available Slot Found!'], 'status' =>
+                422]);
+            }
+        } else {
+            return response()->json(['errors' => ['Doctor is not available in selected date or time.
+            Please select another doctor or check the availability of the doctor.'], 'status' =>
+            422]);
+        }
+    }
+
+    function cancelAppointment(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'TempId' => 'required|integer',
             'PatientId' => 'required|integer',
@@ -259,7 +302,7 @@ class PatientController extends Controller
             return response()->json(['errors' => $validator->errors(), 'status' => 422]);
         }
         try {
-            $data = DB::statement('EXEC usp_api_DeleteAppointment ?, ?, ?, ?, ?', [
+            $data = DB::statement('EXEC usp_app_api_DeleteAppointment ?, ?, ?, ?, ?', [
                 // 96060,
                 $request->TempId,
                 // 24894,
@@ -270,11 +313,10 @@ class PatientController extends Controller
                 $request->APPDate,
                 1
             ]);
-            if($data)
-            {
-                return response()->json(['message'=>'Your Appointment has been Deleted Successfully!', 'status' => 200]);
-            }else{
-            return response()->json(['errors' => $data, 'status' => 500]);
+            if ($data) {
+                return response()->json(['message' => 'Your Appointment has been Deleted Successfully!', 'status' => 200]);
+            } else {
+                return response()->json(['errors' => $data, 'status' => 500]);
             }
         } catch (\Throwable $th) {
             throw $th;
